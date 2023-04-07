44db733d66039642c5931d54162e7e854e48074f
Oh-SulGi <114970679+Oh-SulGi@users.noreply.github.com>
2023-04-07 09:33:20 +0900

    4/6 update

--- Work_SG/cp2_get_obj.py
+++ Work_SG/cp2_get_obj.py
@@ -45,8 +45,9 @@ def save_local(data):
     불러온 json 데이터를 로컬에 저장하는 함수
     '''
     import json
-
-    with open('CP2/data.json', 'w', encoding='utf-8') as f:
+    
+    path = 'CP2/data.json'
+    with open(path, 'w', encoding='utf-8') as f:
         json.dump(json_data, f, ensure_ascii=False, indent=4)
 
 save_local(json_data)
